59b8400e78b206dfdd65469cd6bcde831426b947
Dratatin <matchirouze@orange.fr>
2022-11-08 11:06:01 +0100

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style//main.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="light">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
